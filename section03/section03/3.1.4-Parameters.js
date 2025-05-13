1aacc98ff54991cd1a1c183458d72edbee060e6b
INGRoyer <mitchellyroyer@gmail.com>
2025-05-13 09:29:03 -0500

    classActivities

---
+++ section03/section03/3.1.4-Parameters.js
@@ -0,0 +1,25 @@
+// Named Parameters
+// write a function that takes two named parameters:
+// print each named parameter,
+// then return the parameters added together
+function putParametersTogether(param1,param2){
+  console.log(param1,param2);
+  return param1 + param2;
+}
+
+putParametersTogether('3', '5')
+// invoke the function and pass in two numbers
+putParametersTogether(3, 5);
+// invoke the function and pass in more than two numbers
+putParametersTogether(3, 5, 7);
+// invoke the function and pass in only one number
+putParametersTogether(3, 5);
+// change the function to set default values for the parameters
+
+// again, invoke the function and pass in only one number
+
+// Rest Operator
+// add a rest operator to the function's parameters
+// add a line to the function's body to print the value of the rest operator
+
+// again, invoke the function and pass in more than two numbers
